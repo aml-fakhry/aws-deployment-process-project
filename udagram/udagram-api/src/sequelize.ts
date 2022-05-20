a138309a0e8ffe5e95c3f9a7f4ede24afc900c36
Aml Fakhri <amlfakhry13@gmail.com>
2022-05-21 00:36:46 +0200

    modify gitignore and build script

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,5 +1,5 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 export const sequelize = new Sequelize({
   username: config.username,
@@ -7,7 +7,6 @@ export const sequelize = new Sequelize({
   database: config.database,
   host: config.host,
   port: config.port,
-
-  dialect: "postgres",
-  storage: ":memory:",
+  dialect: 'postgres',
+  storage: ':memory:',
 });
